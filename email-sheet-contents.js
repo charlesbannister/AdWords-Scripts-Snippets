2f8fe200d5ea3f18245d052d88db55ff3f22a12b
Automating AdWords <charles@automatingadwords.com>
2018-01-02 04:21:35 +0000

    added snippet for normal messages (no results)

--- email-sheet-contents.js
+++ email-sheet-contents.js
@@ -70,3 +70,55 @@ function decimalAdjust(type, value, exp) {
       return decimalAdjust('round', value, exp);
     };
   }
+
+
+//snippet for emailing a message along with the sheet settings
+
+//code to go in main()
+  //create email message
+ var msg = "<div style='line-height:1em;'><p>Hi,</p><p>Please find below, a summary of logs and/or changes for the account '"+SETTINGS.NAME+ "'.</p><p><br>"
+  msg+="<strong>Shared Negative List Name: </strong>'" + SETTINGS.LIST_NAME+"'.</p><p>"
+  msg+="<strong>Number of negative keyword additions: </strong>" + numberOfAdditions+".</p><p>"
+  msg+="<strong>Number of negative keyword removals: </strong>" + numberOfRemovals+".</p>"
+  msg+= "<p><strong>Numer of campaigns the shared list was assigned to: </strong>" + numberOfAssignedCampaigns + " (NB: This is all campaigns which matched the filters, not just new additions).</p>"
+  msg+= "<p>The settings for this row can be found on <a href='"+INPUT_SHEET_URL+"'>the control sheet</a> and below:</p><br><br>"
+   msg += settingsToTable(SETTINGS,3,logsColumn-1, controlSheet)
+   msg += "</div>"
+  log(msg)
+  
+  //finally, send an email
+  emailSheet(SETTINGS, msg)
+
+
+//accompanying functions
+function settingsToTable(SETTINGS,headerRow,lastColumn, controlSheet){//settings, the row the headers are on, and the column the settings end at
+  var columnTitles = controlSheet.getRange(headerRow, 1, 1, lastColumn).getValues()[0]
+  var settings = controlSheet.getRange(SETTINGS.ROW_NUM, 1, 1, lastColumn).getValues()[0]
+  var notes = controlSheet.getRange(headerRow, 1, 1, lastColumn).getNotes()[0]
+  //create html table
+  var table = "<table style='background-color:white;border-collapse:collapse;' border = 1 cellpadding = 5>  <tr>    <th>Setting</th>    <th>Value</th>     <th>Notes</th>  </tr>"
+  for(var c in columnTitles){
+    table+="<tr>"
+    table+="<td>"+columnTitles[c]+"</td>"
+    table+="<td>"+settings[c]+"</td>"
+    table+="<td>"+notes[c]+"</td>"
+    table+="</tr>"
+  }
+  table+="</table>"
+  return table
+}
+
+function emailSheet(SETTINGS, message) {
+
+  var emails = SETTINGS.EMAIL
+  var subject = SETTINGS.NAME + " - "+ INPUT_TAB_NAME;
+  
+    for(var email_i in emails){
+   MailApp.sendEmail({
+     to: emails[email_i],
+     subject: subject,
+     htmlBody: message  
+   });
+    }
+}
+
